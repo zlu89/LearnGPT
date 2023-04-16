bc9a0bc5d8efd2b46406fba7e9da94b026012265
zlu89 <44482456+zlu89@users.noreply.github.com>
2023-04-16 16:31:26 -0500

    Update Contact.js

--- Page Javascripts/Contact.js
+++ Page Javascripts/Contact.js
@@ -1,15 +1,35 @@
 // Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+import { authentication, currentMember } from 'wix-members';
+import wixLocation from 'wix-location';
+import {sendEmail} from 'backend/email';
 
 $w.onReady(function () {
 
-    // Write your Javascript code here using the Velo framework API
+	// Write your Javascript code here using the Velo framework API
 
-    // Print hello world:
-    // console.log("Hello world!");
+	// Print hello world:
+	// console.log("Hello world!");
+  $w("#button1").onClick(() =>sendFormData());
 
-    // Call functions on page elements, e.g.:
-    // $w("#button1").label = "Click me!";
+	// Call functions on page elements, e.g.:
+	// $w("#button1").label = "Click me!";
 
-    // Click "Run", or Preview your site, to execute your code
+	// Click "Run", or Preview your site, to execute your code
 
 });
+
+
+
+function sendFormData() {
+  const subject = 'New Submission from ${$w("#input1").value}';
+  const body = '{firstName: ${$w("#input5").value}';
+  let email= $w('#input4').value
+  const name =$w('#input5').value
+  sendEmail(subject, body,email,name)
+    .then(response => console.log(response)); 
+}
+
+authentication.onLogin((user)=>{
+    wixLocation.to("/");
+    }
+)
