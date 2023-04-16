7c6c7b8107b08526181e0c223db1ff4332fab2ae
zlu89 <44482456+zlu89@users.noreply.github.com>
2023-04-16 16:32:47 -0500

    Update ChatGPT videos.js

--- Page Javascripts/ChatGPT videos.js
+++ Page Javascripts/ChatGPT videos.js
@@ -1,15 +1,22 @@
 // Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-
+import { authentication, currentMember } from 'wix-members';
+import wixLocation from 'wix-location';
 $w.onReady(function () {
 
-    // Write your Javascript code here using the Velo framework API
+	// Write your Javascript code here using the Velo framework API
 
-    // Print hello world:
-    // console.log("Hello world!");
+	// Print hello world:
+	// console.log("Hello world!");
 
-    // Call functions on page elements, e.g.:
-    // $w("#button1").label = "Click me!";
+	// Call functions on page elements, e.g.:
+	// $w("#button1").label = "Click me!";
 
-    // Click "Run", or Preview your site, to execute your code
+	// Click "Run", or Preview your site, to execute your code
 
 });
+
+
+authentication.onLogin((user)=>{
+    wixLocation.to("/");
+    }
+)
